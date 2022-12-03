122345246464b94024e639f75e96166b1afef5eb
federicomontero86 <104212036+federicomontero86@users.noreply.github.com>
2022-12-03 16:34:03 -0300

    fix commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-  const randomNumber = Math.random() * 10;
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
   if (randomNumber <= 3) {
     return "Rock";
   } else if (randomNumber > 3 && randomNumber <= 6) {
@@ -8,75 +8,109 @@ function computerPlay() {
 }
 // console.log(computerPlay());
 
+function userPlay() {
+  const playerInput = prompt("Choose Rock, Paper or Scissors!");
+  if (!playerInput) {
+    return "No value";
+  } else if (
+    playerInput.toLowerCase() !== "rock" &&
+    playerInput.toLowerCase() !== "paper" &&
+    playerInput.toLowerCase() !== "scissors"
+  ) {
+    return "Invalid option";
+  } else return playerInput;
+}
+// console.log(userPlay());
+
 function playRound(playerSelection, computerSelection) {
   const playerCaseInsensitive = playerSelection.toLowerCase();
   const computerCaseInsensitive = computerSelection.toLowerCase();
-  if (playerCaseInsensitive === computerCaseInsensitive) {
-    return "We have a Draw, try again!";
+  if (playerCaseInsensitive === "no value") {
+    return "Please type an option!";
+  } else if (playerCaseInsensitive === "invalid option") {
+    return "Please select a valid option!";
+  } else if (playerCaseInsensitive === computerCaseInsensitive) {
+    return "We have a draw ⚖️";
   } else if (
     computerCaseInsensitive === "rock" &&
     playerCaseInsensitive === "paper"
   ) {
-    return "You Win! Paper beats Rock";
+    return "You Win! Paper beats Rock 🥳🥳🥳";
   } else if (
     computerCaseInsensitive === "rock" &&
     playerCaseInsensitive === "scissors"
   ) {
-    return "You lose! Rock beats Scissors";
+    return "You lose! Rock beats Scissors 😭";
   } else if (
     computerCaseInsensitive === "paper" &&
     playerCaseInsensitive === "rock"
   ) {
-    return "You lose! Paper beats Rock";
+    return "You lose! Paper beats Rock 😭";
   } else if (
     computerCaseInsensitive === "paper" &&
     playerCaseInsensitive === "scissors"
   ) {
-    return "You win! Scissors beats paper";
+    return "You win! Scissors beats paper 🥳🥳🥳";
   } else if (
     computerCaseInsensitive === "scissors" &&
     playerCaseInsensitive === "rock"
   ) {
-    return "You win! Rock beats Scissors";
+    return "You win! Rock beats Scissors 🥳🥳🥳";
   } else {
-    return "You lose! Scissors beats Paper";
+    return "You lose! Scissors beats Paper 😭";
   }
 }
 
-const playerSelection = "rock";
 const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
+const playerSelection = "rock";
 
 function game() {
   let userScore = 0;
   let computerScore = 0;
-  alert("Let's play Rock Scissors Paper Game!");
+  alert("Let's play Rock Scissors Paper Game against the Computer!");
   for (let i = 0; i < 5; i++) {
-    const playerInput = prompt("Choose Rock, Paper or Scissors!");
-    if (!playerInput) {
-      return alert("Please instert an option!");
-    } else if (
-      playerInput.toLowerCase() !== "rock" &&
-      playerInput.toLowerCase() !== "paper" &&
-      playerInput.toLowerCase() !== "scissors"
-    ) {
-      return "Please insert a correct option!";
-    } else if (
-      playRound(playerInput, computerSelection) === "We have a Draw, try again!"
-    ) {
-      return;
-    } else if (
-      playRound(playerInput, computerSelection) ===
-        "You Win! Paper beats Rock" ||
-      playRound(playerInput, computerSelection) ===
-        "You win! Scissors beats paper" ||
-      playRound(playerInput, computerSelection) ===
-        "You win! Rock beats Scissors"
-    ) {
+    // console.log(playRound(playerSelection(), computerPlay()));
+    const roundResult = playRound(userPlay(), computerPlay());
+    if (roundResult.includes("win!")) {
       userScore++;
-    } else computerScore++;
-
-    alert(`Your Score: ${userScore}\nComputer Score: ${computerScore}`);
+    } else if (roundResult.includes("lose!")) {
+      computerScore++;
+    } else if (roundResult.includes("draw")) {
+      userScore++;
+      computerScore++;
+    }
+    console.log(
+      `ROUND ${
+        i + 1
+      }\n${roundResult}\nYour Score: ${userScore}\nComputer Score: ${computerScore}`
+    );
+    alert(
+      `ROUND ${
+        i + 1
+      }\n${roundResult}\nYour Score: ${userScore}\nComputer Score: ${computerScore}`
+    );
+  }
+  if (userScore > computerScore) {
+    console.log(
+      `Congratulations 🥳🥳\nYou have defeated the Computer!!\nFinal Result: ${userScore}-${computerScore}`
+    );
+    alert(
+      `Congratulations 🥳🥳\nYou have defeated the Computer!!\nFinal Result: ${userScore}-${computerScore}`
+    );
+  } else if (userScore < computerScore) {
+    console.log(
+      `Computer wins 🖥️\nMybe the next time ;)\nFinal Result: ${userScore}-${computerScore}`
+    );
+    alert(
+      `Computer wins 🖥️\nMybe the next time ;)\nFinal Result: ${userScore}-${computerScore}`
+    );
+  } else {
+    console.log(
+      `We have a draw!! Let's try again!!\nFinal Result: ${userScore}-${computerScore}`
+    );
+    alert(
+      `We have a draw!! Let's try again!!\nFinal Result: ${userScore}-${computerScore}`
+    );
   }
 }
-console.log(game());
+game();
